3105856066067a43b6a52a50518516a21df2e5dd
ghubcoder <ghubcoder@users.noreply.github.com>
2018-04-01 10:52:54 +0100

    Delete firewall rules before lb

--- do/loadbalancers.go
+++ do/loadbalancers.go
@@ -350,13 +350,12 @@ func (l *loadbalancers) EnsureLoadBalancerDeleted(clusterName string, service *v
 		return err
 	}
 
-	_, err = l.client.LoadBalancers.Delete(ctx, lb.ID)
-
+    err = l.EnsureFirewallDeleted(lb)
 	if err != nil {
 		return err
 	}
 
-    err = l.EnsureFirewallDeleted(lb)
+	_, err = l.client.LoadBalancers.Delete(ctx, lb.ID)
     return err
 }
 
