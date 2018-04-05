577d62ae939df2479ceda3643ebb2643a7b297f9
Fatih Arslan <ftharsln@gmail.com>
2018-04-05 17:01:48 +0300

    csi: add CreateVolume

--- do/csi/driver/controller.go
+++ do/csi/driver/controller.go
@@ -5,11 +5,72 @@ import (
 	"errors"
 
 	csi "github.com/container-storage-interface/spec/lib/go/csi/v0"
+	"github.com/digitalocean/godo"
+)
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+)
+
+const (
+	defaultVolumeSizeInGB = 16 * GB
+
+	defaultMinVolumeSizeInGB = 1 * GB
+	defaultMaxVolumeSizeInGB = 16 * TB
 )
 
 // CreateVolume creates a new volume
-func (d *Driver) CreateVolume(context.Context, *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
-	return nil, errors.New("not implemented")
+func (d *Driver) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
+	volumeName := req.Name
+
+	// get volume first
+	//  3. if created, do nothing
+	//  2. if not created, create it
+	volumes, _, err := d.doClient.Storage.ListVolumes(ctx, &godo.ListVolumeParams{
+		Region: d.region,
+		Name:   volumeName,
+	})
+	// TODO(arslan): return if error is *not* type "not found"
+	if err == nil {
+		// TODO(arslan): what happens if there are multiple volumes with the
+		// same name, possible? Check it out
+		vol := volumes[0]
+
+		return &csi.CreateVolumeResponse{
+			Volume: &csi.Volume{
+				Id:            vol.ID,
+				CapacityBytes: vol.SizeGigaBytes * GB,
+			},
+		}, nil
+	}
+
+	size, err := extractStorage(req.CapacityRange)
+	if err != nil {
+		return nil, err
+	}
+
+	volumeReq := &godo.VolumeCreateRequest{
+		Region:        d.nodeId,
+		Name:          volumeName,
+		Description:   "Created by DigitalOcean CSI driver",
+		SizeGigaBytes: size,
+	}
+
+	vol, _, err := d.doClient.Storage.CreateVolume(ctx, volumeReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return &csi.CreateVolumeResponse{
+		Volume: &csi.Volume{
+			Id:            vol.ID,
+			CapacityBytes: size * GB,
+		},
+	}, nil
 }
 
 // DeleteVolume deletes the given volume. This doesn't mean it needs to be
@@ -47,4 +108,22 @@ func (d *Driver) GetCapacity(context.Context, *csi.GetCapacityRequest) (*csi.Get
 // ControllerGetCapabilities ...
 func (d *Driver) ControllerGetCapabilities(context.Context, *csi.ControllerGetCapabilitiesRequest) (*csi.ControllerGetCapabilitiesResponse, error) {
 	return nil, errors.New("not implemented")
+}
+
+// extractStorage extracts the storage size in GB from the given capacity
+// range. If the capacity range is not satisfied it returns the default volume
+// size.
+func extractStorage(capRange *csi.CapacityRange) (int64, error) {
+	if capRange == nil {
+		return defaultVolumeSizeInGB, nil
+	}
+
+	minSize := (capRange.RequiredBytes) / GB
+	maxSize := (capRange.LimitBytes) / GB
+
+	if minSize == maxSize {
+		return minSize, nil
+	}
+
+	return 0, errors.New("requiredBytes and LimitBytes are not the same")
 }
