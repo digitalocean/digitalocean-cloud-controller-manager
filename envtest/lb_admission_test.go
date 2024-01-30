023ef06898a2cf13319729779da3e90627be2439
Philippe Richard <prichard@digitalocean.com>
2024-01-30 15:57:58 -0500

    feat(CON-7852): add LB admission webhook integration tests (#682)
    * feat(CON-7852): add LB admission webhook integration tests
    
    This change adds integration tests for the admission webhook introduced
    in #676. By leveraging envtest, we can test the flow of creation and
    update with the Kubernetes API server.

---
+++ envtest/lb_admission_test.go
@@ -0,0 +1,247 @@
+//go:build integration
+// +build integration
+
+/*
+Copyright 2024 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package envtest
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/digitalocean/godo"
+	"github.com/google/uuid"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func TestLBAdmissionWebhook(t *testing.T) {
+	testCases := []struct {
+		name            string
+		givenService    *corev1.Service
+		givenLBResponse *stubLBResponse
+		allow           bool
+	}{
+		{
+			name: "allowed when service type is not LB",
+			givenService: fakeService(nil, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeNodePort,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenLBResponse: nil,
+			allow:           true,
+		}, {
+			name: "failed when annotation contains unallowed value",
+			givenService: fakeService(map[string]string{
+				"service.beta.kubernetes.io/do-loadbalancer-protocol": "udp",
+			}, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenLBResponse: nil,
+			allow:           false,
+		}, {
+			name: "allowed when godo returns a transiant error",
+			givenService: fakeService(map[string]string{
+				"service.beta.kubernetes.io/do-loadbalancer-protocol": "tcp",
+			}, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenLBResponse: fakeGodoResp(http.StatusInternalServerError, errors.New("random error")),
+			allow:           true,
+		}, {
+			name: "denied when godo returns a bad request error",
+			givenService: fakeService(map[string]string{
+				"service.beta.kubernetes.io/do-loadbalancer-protocol": "tcp",
+			}, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenLBResponse: fakeGodoResp(http.StatusBadRequest, errors.New("random error")),
+			allow:           false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			lbStub.setCreateResp(tc.givenService.Name, tc.givenLBResponse)
+			err := k8sClient.Create(ctx, tc.givenService)
+			if tc.allow && err != nil {
+				t.Errorf("expected allowed admission request, got %q", err)
+			}
+			if !tc.allow && err == nil {
+				t.Error("expected denied admission request, received no error")
+			}
+		})
+	}
+}
+
+func TestLBAdmissionWebhook_Update(t *testing.T) {
+	testCases := []struct {
+		name                  string
+		givenService          *corev1.Service
+		givenUpdate           func(s *corev1.Service)
+		givenLBUpdateResponse *stubLBResponse
+		allow                 bool
+	}{
+		{
+			name: "failed when annotation contains unallowed value",
+			givenService: fakeService(map[string]string{
+				"service.beta.kubernetes.io/do-loadbalancer-protocol": "tcp",
+			}, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenUpdate: func(s *corev1.Service) {
+				s.Annotations["service.beta.kubernetes.io/do-loadbalancer-protocol"] = "udp"
+			},
+			givenLBUpdateResponse: nil,
+			allow:                 false,
+		}, {
+			name: "allowed when godo returns a transiant error",
+			givenService: fakeService(map[string]string{
+				"service.beta.kubernetes.io/do-loadbalancer-protocol": "tcp",
+			}, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenUpdate: func(s *corev1.Service) {
+				s.Annotations["service.beta.kubernetes.io/do-loadbalancer-protocol"] = "http"
+			},
+			givenLBUpdateResponse: fakeGodoResp(http.StatusInternalServerError, errors.New("random error")),
+			allow:                 true,
+		}, {
+			name: "denied when godo returns a bad request error",
+			givenService: fakeService(map[string]string{
+				"service.beta.kubernetes.io/do-loadbalancer-protocol": "tcp",
+			}, &corev1.ServiceSpec{
+				Type: corev1.ServiceTypeLoadBalancer,
+				Ports: []corev1.ServicePort{
+					{
+						Name:       "http",
+						Protocol:   "TCP",
+						Port:       80,
+						TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+					},
+				},
+			}),
+			givenUpdate: func(s *corev1.Service) {
+				s.Annotations["service.beta.kubernetes.io/do-loadbalancer-protocol"] = "http"
+			},
+			givenLBUpdateResponse: fakeGodoResp(http.StatusBadRequest, errors.New("random error")),
+			allow:                 false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			svc := tc.givenService
+
+			lbStub.setCreateResp(svc.Name, fakeGodoResp(http.StatusOK, nil))
+			k8sClient.Create(ctx, svc)
+
+			tc.givenUpdate(svc)
+			lbStub.setUpdateResp(svc.Name, tc.givenLBUpdateResponse)
+			err := k8sClient.Update(ctx, svc)
+			if tc.allow && err != nil {
+				t.Errorf("expected allowed admission request, got %q", err)
+			}
+			if !tc.allow && err == nil {
+				t.Error("expected denied admission request, received no error")
+			}
+		})
+	}
+}
+
+func fakeService(annotations map[string]string, spec *corev1.ServiceSpec) *corev1.Service {
+	lbID := uuid.NewString()
+	serviceName := fmt.Sprintf("svc-%s", lbID)
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+	annotations["kubernetes.digitalocean.com/load-balancer-id"] = lbID
+	annotations["service.beta.kubernetes.io/do-loadbalancer-name"] = serviceName
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        serviceName,
+			Namespace:   "default",
+			Annotations: annotations,
+		},
+		Spec: *spec,
+	}
+}
+
+func fakeGodoResp(status int, err error) *stubLBResponse {
+	return &stubLBResponse{
+		lb:  nil,
+		r:   &godo.Response{Response: &http.Response{StatusCode: status}},
+		err: err,
+	}
+}
---
+++ envtest/suite_test.go
@@ -0,0 +1,192 @@
+//go:build integration
+// +build integration
+
+/*
+Copyright 2024 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package envtest
+
+import (
+	"context"
+	"errors"
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/digitalocean/digitalocean-cloud-controller-manager/cloud-controller-manager/do"
+	"github.com/digitalocean/godo"
+	admissionv1 "k8s.io/api/admissionregistration/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	crlog "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	k8sClient client.Client
+	lbStub    *godoLBStub
+)
+
+func TestMain(m *testing.M) {
+	log := zap.New(zap.WriteTo(os.Stdout), zap.UseDevMode(true))
+	crlog.SetLogger(log)
+	log = log.WithName("test_main")
+
+	log.Info("configuring test env")
+	webhookInstallOptions := &envtest.WebhookInstallOptions{
+		ValidatingWebhooks: []*admissionv1.ValidatingWebhookConfiguration{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "validation-webhook.loadbalancer.doks.io",
+				},
+				Webhooks: []admissionv1.ValidatingWebhook{
+					{
+						Name:           "validation-webhook.loadbalancer.doks.io",
+						FailurePolicy:  ptr.To(admissionv1.Ignore),
+						SideEffects:    ptr.To(admissionv1.SideEffectClassNone),
+						TimeoutSeconds: ptr.To[int32](10),
+						Rules: []admissionv1.RuleWithOperations{
+							{
+								Operations: []admissionv1.OperationType{admissionv1.Create, admissionv1.Update},
+								Rule: admissionv1.Rule{
+									APIGroups:   []string{"*"},
+									APIVersions: []string{"v1"},
+									Resources:   []string{"services"},
+									Scope:       ptr.To(admissionv1.AllScopes),
+								},
+							},
+						},
+						AdmissionReviewVersions: []string{"v1"},
+						ClientConfig: admissionv1.WebhookClientConfig{
+							Service: &admissionv1.ServiceReference{
+								Path: ptr.To("/lb-service"),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	testEnv := &envtest.Environment{
+		WebhookInstallOptions: *webhookInstallOptions,
+	}
+
+	log.Info("setting up fake godo client")
+	godoClient := godo.NewFromToken("")
+	lbStub = &godoLBStub{
+		createResponses: make(map[string]*stubLBResponse),
+		updateResponses: make(map[string]*stubLBResponse),
+	}
+	godoClient.LoadBalancers = lbStub
+
+	log.Info("starting admission server")
+	cfg, err := testEnv.Start()
+	if err := webhookInstallOptions.ModifyWebhookDefinitions(); err != nil {
+		log.Error(err, "failed to initialize webhook definition")
+		os.Exit(1)
+	}
+
+	log.Info("setting up webhook server")
+	server := webhook.NewServer(webhook.Options{
+		Host:    testEnv.WebhookInstallOptions.LocalServingHost,
+		Port:    testEnv.WebhookInstallOptions.LocalServingPort,
+		CertDir: testEnv.WebhookInstallOptions.LocalServingCertDir,
+	})
+	server.Register("/lb-service", &webhook.Admission{Handler: do.NewLBServiceAdmissionHandler(&log, godoClient)})
+
+	//+kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	if err != nil {
+		log.Error(err, "failed to initialize k8s client")
+		os.Exit(1)
+	}
+
+	log.Info("starting webhook server")
+	ctx, cancelWebhookServer := context.WithCancel(context.Background())
+	var webhookServerWg sync.WaitGroup
+	webhookServerWg.Add(1)
+	go func() {
+		defer webhookServerWg.Done()
+		if err := server.Start(ctx); err != nil {
+			log.Error(err, "failed to serve webhook server")
+			cancelWebhookServer()
+		}
+	}()
+
+	log.Info("executing tests")
+	code := m.Run()
+
+	log.Info("tearing down test env")
+	cancelWebhookServer()
+	webhookServerWg.Wait()
+	if err := testEnv.Stop(); err != nil {
+		log.Error(err, "failed shutting down envtest gracefully")
+		os.Exit(1)
+	}
+
+	os.Exit(code)
+}
+
+type stubLBResponse struct {
+	lb  *godo.LoadBalancer
+	r   *godo.Response
+	err error
+}
+
+type godoLBStub struct {
+	godo.LoadBalancersService
+	createResponses map[string]*stubLBResponse
+	updateResponses map[string]*stubLBResponse
+	mtx             sync.Mutex
+}
+
+func (f *godoLBStub) setCreateResp(name string, r *stubLBResponse) {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	f.createResponses[name] = r
+}
+
+func (f *godoLBStub) setUpdateResp(name string, r *stubLBResponse) {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	f.updateResponses[name] = r
+}
+
+func (f *godoLBStub) Create(ctx context.Context, lbr *godo.LoadBalancerRequest) (*godo.LoadBalancer, *godo.Response, error) {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	r, ok := f.createResponses[lbr.Name]
+	if !ok {
+		return nil, nil, errors.New("expected resp not found")
+	}
+	return r.lb, r.r, r.err
+}
+
+func (f *godoLBStub) Update(ctx context.Context, lbID string, lbr *godo.LoadBalancerRequest) (*godo.LoadBalancer, *godo.Response, error) {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	r, ok := f.updateResponses[lbr.Name]
+	if !ok {
+		return nil, nil, errors.New("expected resp not found")
+	}
+	return r.lb, r.r, r.err
+}
